13eebf15ef7955651474b2c27456e48a60eae0b5
Eshan Singh Awal <singheshan29@gmail.com>
2022-07-24 13:16:01 +0530

    post request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const {state, setState} = useState(0)
+  
+  
+  useEffect(()=>{
+    
+    const url ="https://emkc.org/api/v2/piston/runtimes"
+    const postUrl = "https://emkc.org/api/v2/piston/execute"
+    // console.log({codeFile});
+    const newdataa = {language: "python2",
+    version: "2.7.18",
+    files: [
+      {content:"print(5)"},          
+    ],
+    compile_timeout: 10000,
+    run_timeout: 3000,
+    compile_memory_limit: -1,
+    run_memory_limit: -1}
+
+    //post 
+    const postPiston = () => {
+      fetch(postUrl, {
+        method: 'POST',
+        //media type of resource
+        header: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newdataa),
+       
+      })
+      .then((response) => response.json())
+      //Then with the data from the response in JSON...
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      //Then with the error genereted...
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+      
+    }
+    postPiston();
+  }, )
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={null}></button>
       </header>
     </div>
   );
---
+++ src/code.js
@@ -0,0 +1,3 @@
+[{
+    content:
+    "print(5);"}]
