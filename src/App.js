1898ebac8cc5bd6acadaba4ec594593d19d1ab2f
Eshan Singh Awal <singheshan29@gmail.com>
2022-07-24 16:01:02 +0530

    using piston to compile

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function App() {
-  const {state, setState} = useState(0)
+//use state can only be initialised inside react component state or app function
   
-  
-  useEffect(()=>{
-    
-    const url ="https://emkc.org/api/v2/piston/runtimes"
-    const postUrl = "https://emkc.org/api/v2/piston/execute"
-    // console.log({codeFile});
-    const newdataa = {language: "python2",
-    version: "2.7.18",
-    files: [
-      {content:"print(5)"},          
-    ],
-    compile_timeout: 10000,
-    run_timeout: 3000,
-    compile_memory_limit: -1,
-    run_memory_limit: -1}
 
-    //post 
-    const postPiston = () => {
+
+function App() {
+  const [code, setCode] = useState("print(5)")
+ 
+  //post function
+    function postPiston() {  
+      const newdataa = {language: "python2",
+      version: "2.7.18",
+      files: [
+        {content:code},          
+      ],
+      compile_timeout: 10000,
+      run_timeout: 3000,
+      compile_memory_limit: -1,
+      run_memory_limit: -1}
+
+      const url ="https://emkc.org/api/v2/piston/runtimes"
+      const postUrl = "https://emkc.org/api/v2/piston/execute"
+
       fetch(postUrl, {
         method: 'POST',
         //media type of resource
@@ -41,12 +42,18 @@ function App() {
       });
       
     }
-    postPiston();
-  }, )
+
+    const handleCodeChange = (event) =>{
+      setCode(event.target.value);
+      console.log(code);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={null}></button>
+      <label htmlFor='Message'>MEssage</label>
+        <textarea rows={4} cols={50} id="Message" value={code} onChange={handleCodeChange}></textarea>
+        <button onClick={postPiston}></button>
       </header>
     </div>
   );
